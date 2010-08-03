a00a9aa4dcd59dc81317983633a1278af0fbf426
Rob Speer <rspeer@mit.edu>
2010-08-03 17:11:24 -0400

    new version with graphing

--- decomp_test.py
+++ decomp_test.py
@@ -7,7 +7,7 @@ from pylab import *
 
 config.ECHO_NEST_API_KEY="LFYSHIOM0NNSDBCKJ"
 
-TEST_FILENAME = "clocks.ogg"
+TEST_FILENAME = "settler.ogg"
 
 def normalize_dense_cols(array):
     array = array.copy()
@@ -22,14 +22,22 @@ def test():
 
     track = Track(TEST_FILENAME)
     bars = [bar['start'] for bar in track.bars]
-    segments = track.segments
-    songdata = np.zeros((len(bars)-1, 26))
+    beats = [beat['start'] for beat in track.beats]
+    meter = float(len(beats))/len(bars)
+    print meter
+    meter = int(np.round(meter))
 
-    for barnum in xrange(len(bars)-1):
-        start = bars[barnum]
-        end = bars[barnum+1]
+    segments = track.segments
+    songdata = np.zeros((len(beats)-1, 27))
+
+    for beatnum in xrange(len(beats)-1):
+        start = beats[beatnum]
+        end = beats[beatnum+1]
+        next_bar = beats[min(beatnum+meter, len(beats)-1)]
         segs = [seg for seg in track.segments if seg['start'] >= start and seg['start'] < end]
-        if segs:
+        bar_segs = [seg for seg in track.segments if seg['start'] >= start and seg['start'] < next_bar]
+        bpm = 60.0/(end-start)
+        if segs or bar_segs:
             pitch = np.zeros((12,))
             timbre = np.zeros((12,))
             loudness = 0
@@ -37,24 +45,30 @@ def test():
                 pitch += np.array(seg['pitches'])
                 timbre += np.array(seg['timbre'])
                 loudness += seg['loudness_max']
-            pitch /= len(segs)
-            timbre /= len(segs)
-            loudness /= len(segs)
+            for seg in bar_segs:
+                pitch += np.array(seg['pitches'])
+                timbre += np.array(seg['timbre'])
+                loudness += seg['loudness_max']
+            n = len(segs)+len(bar_segs)
+            pitch /= n
+            timbre /= n
+            loudness /= n
             segrate = len(segs)
             
-            songdata[barnum,0:12] = pitch
-            songdata[barnum,12:24] = timbre
-            songdata[barnum,24] = loudness
-            songdata[barnum,25] = segrate
-
-        #start_samp = int(bars[barnum] * rate)
-        #end_samp = int(bars[barnum+1] * rate)
-
-        #audiolab.play(snddata[start_samp:end_samp].T)
+            songdata[beatnum,0:12] = pitch
+            songdata[beatnum,12:24] = timbre
+            songdata[beatnum,24] = loudness
+            songdata[beatnum,25] = segrate
+            songdata[beatnum,26] = bpm
 
     divisi_song = divisi2.DenseMatrix(songdata)
     divisi2.save(divisi_song, TEST_FILENAME+'.pickle')
-    return (snddata, bars, rate, normalize_dense_cols(divisi_song))
+    return (snddata, beats, rate, normalize_dense_cols(divisi_song))
 
-snddata, bars, rate, svd = test()
+snddata, beats, rate, mat = test()
+U, S, V = mat.svd(k=10)
+songlen = len(beats)
+colors = [(1.0, x/float(songlen), 0.0) for x in xrange(songlen)]
+plot(U[:,0], U[:,1], 'k-')
+scatter(U[:,0], U[:,1], facecolors=colors, edgecolors=colors)
 
