2d2ace1b32baa5808d9855dfa13bdcfe18cbaf92
Rob Speer <rspeer@mit.edu>
2010-08-13 02:37:23 -0400

    a pretty good decomposition into sawtooth/triangle/noise

--- wavelet.py
+++ wavelet.py
@@ -8,6 +8,30 @@ RATE = 44100
 A0 = 27.5
 M = 65536
 nfilters = 96
+EPS = 1e-8
+ALPHA = 0.5
+
+HARMONIC_VALUES = [(1, 0),
+                   (2, 12),
+                   (3, 19),
+                   (4, 24),
+                   (5, 28),
+                   (6, 31),
+                   (8, 36),
+                   (9, 38),
+                   (10, 40),
+                   (12, 43),
+                   (15, 47),
+                   (16, 48),
+                   (18, 50),
+                   (20, 52),
+                   (24, 55),
+                   (27, 57),
+                   (30, 59),
+                   (32, 60)]
+
+NOISE_SHAPE = hanning(25) / np.sum(hanning(25)) * 3.0
+
 def morlet_freq(f0, M):
     """
     Get a Morlet wavelet tuned to detect a particular frequency.
@@ -84,31 +108,50 @@ def svd_reduce(matrix):
         U_prime[row, col] = 1 #U[row,col]
     return np.dot(U**3, V.T)
 
-from plca.harmonic_plca import HARMONIC_VALUES
-
-EPS = 1e-5
-def deharmonize(matrix):
+def detect_harmonics(matrix, shape):
     F, T = matrix.shape
     alignment = np.zeros((F, T))
     matrix = np.concatenate([matrix, np.ones((60, T))*np.mean(matrix)]) + EPS
-    amplitude = np.sum(matrix, axis=0)
     lmatrix = np.log(matrix)
-    for h, steps in HARMONIC_VALUES:
-        alignment += lmatrix[steps:steps+F] * 1.0/h
-    deharmonized = np.exp(alignment)
-    amp_adjust = np.sum(deharmonized, axis=0) / amplitude
-    return deharmonized / amp_adjust
+    if shape == 'sawtooth':
+        for h, steps in HARMONIC_VALUES:
+            alignment += lmatrix[steps:steps+F] * 1.0/h
+    elif shape == 'triangle':
+        for h, steps in HARMONIC_VALUES:
+            if h % 2 == 1:
+                alignment += lmatrix[steps:steps+F] * 3.2/h/h
+    else:
+        raise NotImplementedError
 
-ALPHA = 0.5
+    deharmonized = np.exp(alignment)
+    return deharmonized
+
+def detect_noise(matrix):
+    F, T = matrix.shape
+    offset = len(NOISE_SHAPE)//2
+    alignment = np.zeros((F, T))
+    matrix = np.concatenate([np.ones((offset, T)) * np.mean(matrix), matrix, np.ones((offset, T)) * np.mean(matrix)]) + EPS
+    lmatrix = np.log(matrix)
+
+    for i, mag in enumerate(NOISE_SHAPE):
+        alignment += lmatrix[i:i+F] * mag
+
+    noise_profile = np.exp(alignment)
+    return noise_profile
+
 meansq = 0.00001
 def timbre_color(matrix):
     # okay, this really is the evil kind of global var. I'll classify sometime
     global meansq
-    deharmonized = deharmonize(matrix)
-    color = np.zeros(deharmonized.shape + (3,))
-    baseline = np.maximum(0, deharmonized)
-    color[:,:,:] = 1
-    rgb = np.maximum(0, baseline[...,np.newaxis] * color)
+    amplitude = np.sum(matrix, axis=0)
+    rgb = np.zeros(matrix.shape + (3,))
+    rgb[:,:,0] = detect_noise(matrix)
+    rgb[:,:,1] = detect_harmonics(matrix, 'sawtooth')
+    rgb[:,:,2] = detect_harmonics(matrix, 'triangle')
+    
+    amp_adjust = np.sum(np.sum(rgb, axis=2), axis=0) / amplitude
+    rgb /= amp_adjust[np.newaxis, :, np.newaxis]
+
     prev_meansq = meansq
     meansq = (meansq*(1.0-ALPHA)) + (np.mean(rgb*rgb) * ALPHA)
     meansq_smooth = np.linspace(np.sqrt(prev_meansq), np.sqrt(meansq), matrix.shape[-1])
@@ -123,7 +166,7 @@ def smooth(matrix, n=20):
 
 if __name__ == '__main__':
     import pylab, time
-    sndfile = audiolab.Sndfile('settler.ogg')
+    sndfile = audiolab.Sndfile('wakinglife.ogg')
     signal = np.mean(sndfile.read_frames(44100*60), axis=1)
     #signal = chirp(np.arange(2**18), 16.3516/44100, 2**18, 4185.01/44100,
     #               method='logarithmic')
